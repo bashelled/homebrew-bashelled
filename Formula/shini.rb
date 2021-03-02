19d99529a486fd0b2aa0a6c7937049336e087bb9
user <user@penguin>
2021-03-02 19:45:27 +0000

    renamed:    yazt.rb -> shini.rb

--- Formula/shini.rb
+++ Formula/shini.rb
@@ -1,10 +1,10 @@
 
-class Yazt < Formula
+class Shini < Formula
   desc "A DIY ZSH theme helper"
   homepage "https://github.com/bashelled/yazt"
-  url "https://github.com/bashelled/yazt/archive/0.4.3-alpha.tar.gz"
-  sha256 "58dfcc6dad4e7b9a13c482ce36bb9ce46246ca4d364db531b469083e79250d85"
+  url "https://github.com/bashelled/yazt/archive/master.tar.gz"
   license "MIT"
+  version "rolling"
 
   def install
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
